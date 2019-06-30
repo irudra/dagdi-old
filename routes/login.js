4dec8042178e581f3a4874d1031a3f21dea279ac
rudra <hemant.gaur11@gmail.com>
2019-06-30 18:27:57 +0530

    added local host for zuul

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 router.post('/', function(req, res){
     
-    request.post({url:'http://192.168.1.3:8079/oauth/token?username=rohit&password=rohit&grant_type=password', headers:{'Accept':'application/json','Authorization':'Basic Y2xpZW50T25lOnBhc3N3b3JkT25l'}}, function callback(error, response, body){
+    request.post({url:'http://localhost:8079/oauth/token?username=rohit&password=rohit&grant_type=password', headers:{'Accept':'application/json','Authorization':'Basic Y2xpZW50T25lOnBhc3N3b3JkT25l'}}, function callback(error, response, body){
         if (error){
             console.log(error);
             throw error;
