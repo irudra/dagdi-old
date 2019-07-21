e3901fc7d18521b91c3bd851f83fb4ed51cccfb7
rudra <hemant.gaur11@gmail.com>
2019-07-21 23:17:16 +0530

    #1 commit

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import * as core from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @ core.Injectable({
-=======
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http' 
-
-  @Injectable({
->>>>>>> be3c48410d2a462383281095ecd1486f63a22f55
     providedIn: 'root'
   })
 
@@ -20,7 +13,6 @@ export class CartService {
   ) { }
 
   addToCart(product){
-<<<<<<< HEAD
     this .items.push(product);
   }
 
@@ -37,21 +29,4 @@ export class CartService {
   return this .http.get("/assets/shipping.json");
   }
 }
-=======
-    this.items.push(product);
-  }
 
-  getItems(){
-    return this.items;
-  }
-
-  clearCart(){
-    this.items = [];
-    return this.items;
-  }
-
-  getShippingPrices(){
-  return this.http.get("/assets/shipping.json");
-  }
-}
->>>>>>> be3c48410d2a462383281095ecd1486f63a22f55
--- src/app/products.ts
+++ src/app/products.ts
@@ -25,4 +25,3 @@ can be found in the LICENSE file at http://angular.io/license
 */
 =======
 */
->>>>>>> be3c48410d2a462383281095ecd1486f63a22f55
